00d94492b683650bbb5cd2683f806a9c3a63ea30
BrunoOller <brunobrunelli4@gmail.com>
2026-02-24 12:44:30 -0300

    Começando um projeto novo em .net mvc

---
+++ DWIII/SistemaAcademico/SistemaAcademico/SistemaAcademico/Models/Aluno.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace SistemaAcademico.Models
+{
+    public class Aluno
+    {
+        public int AlunoId { get; set; }
+        [Display(Name = "RA")]
+        [Required(ErrorMessage = "O RA é Obrigatório!")]
+        [StringLength(10, MinimumLength = 4, ErrorMessage = "O RA deve ter entre 4 e 10 caracteres!")]
+        public string? Ra { get; set; }
+        public int UsuarioId { get; set; }
+
+        public Usuario? Usuario { get; set; }
+    }
+}
